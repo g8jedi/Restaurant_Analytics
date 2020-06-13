35c2f86bacbc5703b4303090d17a5dd850f5b84d
Jairo Batista <jairo.batista@utexas.edu>
2020-06-13 15:37:18 -0500

    Now importing restaurant info from settings file.

--- script.py
+++ script.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
 import datetime
 from time import sleep
 from os import getcwd
 
-from settings import EMAIL, PASSWORD, DRIVER_PATH
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from local_settings import EMAIL, PASSWORD, DRIVER_PATH, LOOP20
 
 
 chrome_options = webdriver.ChromeOptions()
@@ -21,7 +24,7 @@ driver.find_element_by_id("log-in").click()
 sleep(1)
 driver.find_element_by_id("switch-restaurants-menu").click()
 sleep(1)
-driver.find_element_by_link_text("Sushi Madre - Saunders").click()
+driver.find_element_by_link_text(LOOP20.name).click()
 sleep(1)
 driver.get("https://www.toasttab.com/restaurants/admin/reports/home")
 sleep(1)
@@ -32,28 +35,31 @@ date_dropdown.click()
 custom_button = date_dropdown.find_element_by_link_text("Custom Date")
 custom_button.click()
 
-restaurant_first_day = datetime.datetime(2016, 12, 12)
 today = datetime.datetime.today()
-days_delta = (today - restaurant_first_day).days
+days_delta = (today - LOOP20.first_day).days
 
 for i in range(0, days_delta):
     report_date_start = driver.find_element_by_name("reportDateStart")
     driver.execute_script("arguments[0].value = ''", report_date_start)
-    report_date_start.send_keys(restaurant_first_day.strftime("%m-%d-%Y"))
+    report_date_start.send_keys(LOOP20.first_day.strftime("%m-%d-%Y"))
     report_date_start.send_keys(Keys.RETURN)
     sleep(1)
     report_date_end = driver.find_element_by_name("reportDateEnd")
     driver.execute_script("arguments[0].value = ''", report_date_end)
-    report_date_end.send_keys(restaurant_first_day.strftime("%m-%d-%Y"))
+    report_date_end.send_keys(LOOP20.first_day.strftime("%m-%d-%Y"))
     report_date_end.send_keys(Keys.RETURN)
     sleep(2)
     driver.find_element_by_id("report-restaurants").click()
-    sleep(1)
-    driver.find_element_by_id("update-btn").click()
-    sleep(6)
-    summary_content = driver.find_element_by_id("sales-summary-content")
-    export_button = summary_content.find_element_by_css_selector('div a')
-    export_button.click()
     sleep(2)
-    restaurant_first_day += datetime.timedelta(days=1)
-    print(i, restaurant_first_day)
+    driver.find_element_by_id("update-btn").click()
+    try:
+        element = WebDriverWait(driver, 8).until(
+            EC.presence_of_element_located((By.ID, "sales-summary-content"))
+        )
+    finally:
+        summary_content = driver.find_element_by_id("sales-summary-content")
+        export_button = summary_content.find_element_by_css_selector('div a')
+        export_button.click()
+        sleep(1)
+        LOOP20.first_day += datetime.timedelta(days=1)
+        print(i, LOOP20.first_day)
